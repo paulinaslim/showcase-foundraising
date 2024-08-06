789b1eedbef7e88506a9071e07d9e09077998ec6
paulinaslim <paulina.hryszko@weareslim.de>
2024-08-06 08:09:08 +0200

    base

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,12 @@
 const mix = require("laravel-mix");
 const glob = require("glob");
 
-mix.js("resources/js/app.js", "public/js");
+mix.js("resources/js/app.js", "public/js").js(
+    "resources/js/cookieConsent.js",
+    "public/js"
+);
 
-glob.sync("resources/sass/**/*.scss").forEach((file) => {
-    mix.sass(file, "public/css");
-});
+mix.sass("resources/sass/app.scss", "public/css");
 
 mix.options({
     processCssUrls: false,
