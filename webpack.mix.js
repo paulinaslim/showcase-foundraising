69e958eea58ef40be11d7247873dea0a7a3c2225
paulinaslim <paulina.hryszko@weareslim.de>
2024-08-08 14:08:49 +0200

    covert only app.css and app.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,6 +6,12 @@ mix.js("resources/js/app.js", "public/js").js(
     "public/js"
 );
 
+// glob.sync("resources/sass/**/*.scss").forEach((file) => {
+//     if (file !== "resources/sass/app.scss") {
+//         mix.sass(file, "public/css");
+//     }
+// });
+
 mix.sass("resources/sass/app.scss", "public/css");
 
 mix.options({
