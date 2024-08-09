fad5b48f85259aeb851be0436790bdb91f331b38
paulinaslim <paulina.hryszko@weareslim.de>
2024-08-09 08:06:46 +0200

    fixed scrolling bug

--- resources/js/story.js
+++ resources/js/story.js
@@ -1,25 +1,33 @@
-const galleryWrapper = document.getElementById("gallery-wrapper");
-function getScrollAmount() {
-    let galleryWidth = galleryWrapper.scrollWidth;
-    return -(galleryWidth - window.innerWidth);
-}
-const scrollAmount = getScrollAmount();
-console.log("gallery wrapper", galleryWrapper.scrollWidth);
-console.log("scroll distance: ", scrollAmount);
+window.addEventListener("load", function () {
+    const galleryWrapper = document.getElementById("gallery-wrapper");
 
-const tween = gsap.to(galleryWrapper, {
-    x: getScrollAmount,
-    duration: 3,
-    ease: "none",
-});
+    function getScrollAmount() {
+        let galleryWidth = galleryWrapper.scrollWidth;
+        return -(galleryWidth - window.innerWidth);
+    }
 
-ScrollTrigger.create({
-    trigger: "#gallery",
-    start: "top 5%",
-    end: () => `+=${getScrollAmount() * -1}`,
-    pin: true,
-    animation: tween,
-    scrub: 1,
-    invalidateOnRefresh: true,
-    markers: true,
+    console.log("gallery wrapper", galleryWrapper.scrollWidth);
+    console.log("scroll distance: ", getScrollAmount());
+
+    const tl = gsap.timeline();
+
+    tl.to(galleryWrapper, {
+        x: () => getScrollAmount(),
+        duration: 3,
+        ease: "none",
+    }).to(galleryWrapper, {
+        duration: 1,
+    });
+
+    const scrollAmount = Math.abs(getScrollAmount());
+
+    ScrollTrigger.create({
+        trigger: "#gallery",
+        start: "top 5%",
+        end: `+=${scrollAmount}`,
+        pin: true,
+        animation: tl,
+        scrub: 1,
+        invalidateOnRefresh: true,
+    });
 });
